79ed20f6ef617db06b66a28ddd4e82243d1b9249
Vijay.Yeruva <Yeruva.Vijay@gmail.com>
2020-07-03 02:52:52 -0400

    updated points of contact accordion logic

--- js/loadpointsofcontact.js
+++ js/loadpointsofcontact.js
@@ -62,7 +62,7 @@ let buildTabContent = function(distinctAgencies, pointsofcontact){
             tabContent +='<div class="tab-pane fade" id="pills-'+ agencyId +'" role="tabpanel" aria-labelledby="pills-'+ agencyId +'-tab">';
         }
         tabContent += '<div class="sponsor-title-container"><h3 class="sponsor-title"><img class="logo" src="assets/logos/sponsor_logos/'+ agencycontacts[0].acronym.toLowerCase() +'.png">'+ agencycontacts[0].agency.toString() +'</h3></div>';
-        tabContent += buildContacts(agencycontacts);
+        tabContent += buildContacts(agencyId, agencycontacts);
         tabContent += '</div>';
 
     }
@@ -72,9 +72,10 @@ let buildTabContent = function(distinctAgencies, pointsofcontact){
 
 //Start with level1 accordion and build one by one the levels going down.
 //this is nestted accordion that can go upto 4 levels
-let buildContacts = function(agencycontacts){
-    let accordionCounter = 1; 
+let buildContacts = function(agencyId, agencycontacts){
+    let counter = 1; 
     let contactElem = '';
+    contactElem += '<div id = "' + agencyId + '">';
     let level1s = agencycontacts.filter(function(contact){
         return contact.level2 == '';
     });
@@ -90,9 +91,12 @@ let buildContacts = function(agencycontacts){
 
     if(level1as.length > 0)
     {
-        contactElem += '<div class = "accordion" id = "accordionLevel1">';
         let distinctLevel1s = getDistinctAttributes(level1as, 'level1');
         distinctLevel1s.forEach(function(level) {
+            let collapseId1 = "collapse" + counter;
+            let headerId1 = "heading" + counter;
+            let childId1 = "child" + counter;
+            counter++;
             let level2Elem = '';
             //filter level2 without level3
             let level2s = level1as.filter(function(contact){
@@ -110,9 +114,12 @@ let buildContacts = function(agencycontacts){
             //build accordion
             if(level2as.length > 0)
             {
-                level2Elem += '<div class = "accordion" id = "accordionLevel2">';
                 let distinctLevel2s = getDistinctAttributes(level2as, 'level2');
                 distinctLevel2s.forEach(function(level){
+                    let collapseId2 = "collapse" + counter;
+                    let headerId2 = "heading" + counter;
+                    let childId2 = "child" + counter;
+                    counter++;
                     let level3Elem = '';
                     //filter level3 without level4
                     let level3s = level2as.filter(function(contact){
@@ -130,38 +137,30 @@ let buildContacts = function(agencycontacts){
                     //build accordion
                     if(level3as.length > 0)
                     {
-                        level3Elem += '<div class = "accordion" id = "accordionLevel3">';
                         let distinctLevel3s = getDistinctAttributes(level3as, 'level3');
                         distinctLevel3s.forEach(function(level){
+                        let collapseId3 = "collapse" + counter;
+                        let headerId3 = "heading" + counter;
+                        let childId3 = "child" + counter;
+                        counter++;
                         let level4s = level3as.filter(function(contact){
                                 return contact.level3 == level;
                             });
                             let level4Elem = '';
                             level4Elem += buildDivisionElement(level4s, 'level4');
-                            let headerId3 = "collapse" + accordionCounter;
-                            let headingId3 = "heading" + accordionCounter;
-                            level3Elem+= generateAccordionSubElem(3, headerId3, headingId3, level, level4Elem);
-                            accordionCounter++;
+                            level3Elem+= generateAccordionSubElem(3, collapseId3, headerId3, childId2, childId3, level, level4Elem);
                         }); 
-                        level3Elem += '</div>';
                         //end level3 accordion
                     }
-                    let headerId2 = "collapse" + accordionCounter;
-                    let headingId2 = "heading" + accordionCounter;
-                    level2Elem+= generateAccordionSubElem(2, headerId2, headingId2, level, level3Elem);
-                    accordionCounter++;
+                    level2Elem+= generateAccordionSubElem(2, collapseId2, headerId2, childId1, childId2, level, level3Elem);
                 });
-                level2Elem += '</div>';
                 //end level2 accordion
             }  
-            let headerId1 = "collapse" + accordionCounter;
-            let headingId1 = "heading" + accordionCounter;
-            contactElem+= generateAccordionSubElem(1, headerId1, headingId1, level, level2Elem);
-            accordionCounter++;        
+            contactElem+= generateAccordionSubElem(1, collapseId1, headerId1, agencyId, childId1, level, level2Elem);
         });
-        contactElem += '</div>';
-         //end level1 accordion
     }
+    contactElem += '</div>';
+    //end level1 accordion
     return contactElem;
 }
 
@@ -194,4 +193,14 @@ let buildDivisionElement = function(divisions, level){
         content = content + '</ul>';
         return content;
     }
+}
+
+let generateAccordionSubElem = function(level, collapseId, headerId, parentId, childId, header, accordionContent){
+    var headerno = level + 2;
+    let accordionElem =  '<div class = "card"><div class="card-header level'+ level +'" id="'+ headerId + '">' +
+                          '<button class="btn btn-link" data-toggle="collapse" data-target="#'+ collapseId + '" aria-expanded="false" aria-controls="' + collapseId + '">'+
+                            '<h'+ headerno +' class = "content-header-no-margin">' + header + '<i class="fas fa-chevron-down"></i></h'+ headerno +'></button></div>'
+                        + '<div id="'+ collapseId + '" class = "collapse" aria-labelledby= "'+ headerId + '" data-parent="#'+ parentId +'"> <div class = "card-body" id="'+ childId +'">'
+                        + accordionContent +'</div></div></div>';  
+    return accordionElem;
 }
